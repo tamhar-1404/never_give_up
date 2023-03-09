fa0c4917591fc85915f47c216aa9a5842b80f300
Dzzyyy <muhammaddzakydzaky85@gmail.com>
2023-03-09 08:19:58 +0700

    gg

--- resources/views/user_login/cerpen-baik.blade.php
+++ resources/views/user_login/cerpen-baik.blade.php
@@ -5,7 +5,7 @@
 
 <!-- Mirrored from nunforest.com/triptip-demo/single-post.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 08 Feb 2023 10:04:20 GMT -->
 <head>
-	<title>Triptip</title>
+	<title>Cerpen</title>
 
 	<meta charset="utf-8">
 
@@ -36,34 +36,34 @@
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mr-auto">
 					<li>
-						<a class="active" href="/home">Beranda <i class="" aria-hidden="true"></i></a>
-						
-					</li>
-					<li ><a  href="#">Kategori <i class="fa fa-caret-down" aria-hidden="true"></i></a>
-						<div class="megadropdown">
-							<div class="dropdown-box">
+						<a class="active" href="/">Beranda <i class="" aria-hidden="true"></i></a>
 								
-								<ul class="dropdown-list">
-									<li><a href="artikel.html">artikel</a></li>
-									<li><a href="cerpen.html">cerpen</a></li>
-									<li><a href="puisi.html">puisi</a></li>
-									<li><a href="photography.html">fotografi</a></li>
-									</ul>
-							</div>
-							<div class="dropdown-box">
-								<ul class="dropdown-list">
-								
-									<li><a href="makalah.html">makalah</a></li>
-									<li><a href="skripsi.html">skripsi</a></li>
-									<li><a href="ilmiah.html">karya ilmiah</a></li>
-								</ul>
-							</div>
-							<div class="dropdown-box">
-								<ul class="dropdown-list">
-									<li><a href="diary.html">diary</a></li>
-									<li><a href="ilustrasi.html">ilustrasi</a></li>
-									<li><a href="pantun.html">pantun</a></li>
-									<li><a href="essai.html">esai</a></li>
+							</li>
+							<li ><a  href="#">Kategori <i class="fa fa-caret-down" aria-hidden="true"></i></a>
+								<div class="megadropdown">
+									<div class="dropdown-box">
+										
+										<ul class="dropdown-list">
+											<li><a href="/artikel">artikel</a></li>
+											<li><a href="/cerpen">cerpen</a></li>
+											<li><a href="/puisi">puisi</a></li>
+											<li><a href="/photography">fotografi</a></li>
+											</ul>
+									</div>
+									<div class="dropdown-box">
+										<ul class="dropdown-list">
+										
+											<li><a href="/makalah">makalah</a></li>
+											<li><a href="/skripsi">skripsi</a></li>
+											<li><a href="/ilmiah">karya ilmiah</a></li>
+										</ul>
+									</div>
+									<div class="dropdown-box">
+										<ul class="dropdown-list">
+											<li><a href="/diary">diary</a></li>
+											<li><a href="/ilustrasi">ilustrasi</a></li>
+											<li><a href="/pantun">pantun</a></li>
+											<li><a href="/essai">esai</a></li>
 								
 									</ul>
 							</div>
