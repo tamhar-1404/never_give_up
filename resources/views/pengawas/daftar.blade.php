661889a2cab009d15976776745e29c396815e760
naabillaa <127171090+naabillaa@users.noreply.github.com>
2023-03-12 23:23:16 +0700

    pengawas

---
+++ resources/views/pengawas/daftar.blade.php
@@ -0,0 +1,361 @@
+<!doctype html>
+
+
+<html lang="en" class="no-js">
+
+<!-- Mirrored from nunforest.com/triptip-demo/add-listing.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 08 Feb 2023 10:04:22 GMT -->
+<head>
+	<title>Triptip</title>
+
+	<meta charset="utf-8">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<link rel="stylesheet" href="css/triptip-assets.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+
+	<!-- Container -->
+	<div id="container">
+		<!-- Header
+		    ================================================== -->
+			<header class="clearfix dark-header-style fullwidth-with-search">
+
+				<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+					<div class="container">
+	
+                    <a class="navbar-brand" style="width:13%;" href="/user-login">
+							<img src="asset/Logo 1.png" style="width:100%;" alt="">
+						</a>
+	
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+	
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav mr-auto">
+								<li>
+									<a class="" href="/user-login">Beranda <i class="" aria-hidden="true"></i></a>
+								</li>
+								<li ><a  href="#">Kategori <i class="fa fa-caret-down" aria-hidden="true"></i></a>
+								<div class="megadropdown">
+									<div class="dropdown-box">
+										
+										<ul class="dropdown-list">
+											<li><a href="/artikel">artikel</a></li>
+											<li><a href="/cerpen">cerpen</a></li>
+											<li><a href="/puisi">puisi</a></li>
+											<li><a href="/photography">fotografi</a></li>
+											</ul>
+									</div>
+									<div class="dropdown-box">
+										<ul class="dropdown-list">
+										
+											<li><a href="/makalah">makalah</a></li>
+											<li><a href="/skripsi">skripsi</a></li>
+											<li><a href="/ilmiah">karya ilmiah</a></li>
+										</ul>
+									</div>
+									<div class="dropdown-box">
+										<ul class="dropdown-list">
+											<li><a href="/diary">diary</a></li>
+											<li><a href="/ilustrasi">ilustrasi</a></li>
+											<li><a href="/pantun">pantun</a></li>
+											<li><a href="/essai">esai</a></li>
+										
+											</ul>
+									</div>
+
+
+									
+								</div>
+							</li>
+							<li><a href="/contact">Hubungi Kami</a></li>
+						</ul>
+							<a href="/user-login" class="add-list-btn btn-default">Kembali</a>
+	
+					</div>
+				</nav>
+			</header>
+		<!-- End Header -->
+
+		<!-- add-listing
+			================================================== -->
+		<section class="add-listing">
+			<div class="add-listing__title-box">
+				<div class="container">
+					<h1 class="add-listing__title">
+Daftar Menjadi Pengawas					</h1>
+				</div>
+			</div>
+
+			<!-- user scroll menu box -->
+			<!-- <div class="user-detail__scroll-menu-box">
+				<div class="container">
+					<ul class="user-detail__scroll-menu navigate-section">
+						<li>
+							<a class="active" href="#tentang-penulis">Penulis</a>
+						</li>
+						<li>
+							<a  href="#general-info">Postingan</a>
+						</li>
+						<li>
+							<a href="#location-box">Location</a>
+						</li>
+						<li>
+							<a href="#opening-box">Opening Hours</a>
+						</li>
+						<li>
+							<a href="#gallery-box">Gambar</a>
+						</li>
+						<li>
+							<a href="#social-box">Media Sosial</a>
+						</li>
+					</ul>
+				</div>
+			</div> -->
+		<style>
+.add-listing__label > h2{
+	float: left;
+	width: 100px;
+	color: #F072A9;
+	font-weight: bold;
+	font-size: 13px;
+	text-shadow: 1px 1px 1px #fff;
+}
+.add-listing__label label{
+	border-radius: 10px;
+	-webkit-border-radius: 10px;
+	-moz-border-radius: 10px;
+	margin: 0px 0px 10px 0px;
+	border: 1px solid #FFD2D2;
+	padding: 20px;
+	background: #FFF4F4;
+	box-shadow: inset 0px 0px 15px #FFE5E5;
+	-moz-box-shadow: inset 0px 0px 15px #FFE5E5;
+	-webkit-box-shadow: inset 0px 0px 15px #FFE5E5;
+}
+</style> 	
+
+			<!-- form listing -->
+			<form class="" action="" style="color:pink;">
+
+				<div class="container">
+					
+					<!-- form box -->
+					<div class="add-listing__form-box" id="tentang-penulis">
+
+						<h2 class="add-listing__form-title">
+						Isi Data:
+						</h2>
+
+						
+						<div class="add-listing__form-content">
+							<div class="row">
+								<div class="col-md-11">
+									<label class="add-listing__label" for="list-title">
+										Nama :
+									</label>
+									<input class="add-listing__input" type="text" name="list-title" id="list-title" placeholder="Nama" witdh="50px" />
+								</div>
+							</div>
+						<div class="add-listing__form-content">
+							<div class="row">
+								<div class="col-md-11">
+									<label class="add-listing__label" for="list-title">
+										Umur :
+									</label>
+									<input class="add-listing__input" type="number" name="list-title" id="list-title" placeholder="Umur" />
+								</div>
+							</div>
+							
+						
+							
+
+					</div>
+					
+					<!-- form box -->
+
+
+					<!-- form box -->
+
+					<div class="add-listing__form-content">
+						<div class="row">
+							<div class="col-md-11">
+								<label class="add-listing__label" for="list-title">
+									Alamat :
+								</label>
+								<input class="add-listing__input" type="text" name="list-title" id="list-title" placeholder="JL..." />
+							</div>
+						
+
+						<div class="add-listing__form-content">
+							<div class="row">
+								<div class="col-md-11">
+									<label class="add-listing__label" for="list-title">
+										Tanggal Lahir :
+									</label>
+									<input class="add-listing__input" type="date" name="list-title" id="list-title" placeholder="Judul" />
+								</div>
+								<!-- <div class="col-md-6">
+									<label class="add-listing__label" for="category">
+										Kategori:
+									</label>
+									<select class="add-listing__input js-example-basic-multiple" name="category" id="category">
+										<option>Kategori: </option>
+										<option>Artikel</option>
+										<option>Cerpen</option>
+										<option>puisi</option>
+										<option>Fotography</option>
+										<option>Makalah</option>
+										<option>Skripsi</option>
+										<option>Karya Ilmiah</option>
+										<option>Diary</option>
+										<option>Pantun</option>
+										<option>Essai</option>
+									</select>
+								</div> -->
+							</div>	
+							
+						
+							<label class="add-listing__label" for="description">
+								Alasan Ingin Menjadi Pengawas:
+							</label>
+							<textarea  style="resize:none;width: 500px;height:400px;" class="add-listing__textarea" name="description" id="description" placeholder="Tulis Alasan Anda "></textarea>
+						</div>
+
+					
+					
+					<!-- form box -->
+					<div class="add-listing__form-box" id="gallery-box">
+
+						<h2 class="add-listing__form-title">
+							Foto Diri:
+						</h2>
+
+						<div class="add-listing__form-content">
+							<div class="add-listing__input-file-box">
+								<input class="add-listing__input-file" type="file" name="file" id="file"/>
+								<div class="add-listing__input-file-wrap" height="50%"><i>
+								<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
+									<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+									<path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"/>
+									</svg></i>					
+													<p>Ketuk Di Sini untuk Upload Gambar</p>
+								</div>
+							</div>							
+						</div>
+
+					</div>
+					
+					<!-- form box -->
+					
+
+					<div class="center-button" >
+						<button class="add-listing__submit" type="submit" href="artikel.html">
+							<a href="/admin.pengawas"></a><i class="fa fa-paper-plane" aria-hidden="true"></i>
+							Kirim Data 
+						</button></a>
+					</div>
+
+				</div>
+
+			</form>
+		</section>
+		<!-- End add-listing -->
+
+		<!-- footer block module
+			================================================== -->
+			<footer class="footer footer-black">
+				<div class="container">
+	
+					<div class="footer__up-part">
+						<div class="row">
+							<div class="col-md-4">
+	
+								<div class="footer__widget text-widget">
+								<a class="navbar-brand" style="width:50%;" href="/user-login">
+							<img src="asset/Logo 1.png" style="width:100%;" alt="">
+						</a>
+                                <br>
+									<p class="footer__widget-description">
+										Menggembangkan dunia baca tulis menggunakan website menuju dunia digital. Temukan berbagai artikel, postingan, cerita, jurnal dan buat postinganmu sendiri disini	
+									</p>
+								</div>
+	
+							</div>
+							<div class="col-md-4">
+	
+								<div class="footer__widget subscribe-widget">
+									<h2 class="footer__widget-title footer__widget-title-white">
+										Alamat
+									</h2>
+                                    <br>
+									<p class="footer__widget-description">
+										Jl. Pandawa No. 66, Dusun Mawar RT 001 RW 001 Desa Pandan
+									</p>
+									
+								</div>
+	
+							</div>
+							<div class="col-md-4">
+	
+								<div class="footer__widget text-widget">
+									<h2 class="footer__widget-title footer__widget-title-white">
+										Kontak 
+									</h2>
+                                    <br>
+									<p class="footer__widget-description">
+										+62 898-1342-6657<br>
+										info@example.com <br>
+										YouTube: ngu_never122
+									</p>
+								</div>
+	
+							</div>
+						</div>
+					</div>
+	
+					<div class="footer__down-part footer__down-part-black">
+						<div class="row">
+							<div class="col-md-7">
+								<p class="footer__copyright">
+									© Copyright 2018 - All Rights Reserved
+								</p>
+							</div>
+							<div class="col-md-5">
+								<ul class="footer__social-list">
+									<li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+									<li><a class="google" href="#"><i class="fa fa-google-plus"></i></a></li>
+									<li><a class="instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+	
+				</div>
+	
+			</footer>
+		<!-- End footer -->
+
+	</div>
+	<!-- End Container -->
+	
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.migrate.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCiqrIen8rWQrvJsu-7f4rOta0fmI5r2SI"></script>
+	<script src="js/triptip-plugins.min.js"></script>
+	<script src="js/popper.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.countTo.js"></script>
+	<script src="js/script.js"></script>
+	
+</body>
+
+<!-- Mirrored from nunforest.com/triptip-demo/add-listing.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 08 Feb 2023 10:04:22 GMT -->
+</html>
