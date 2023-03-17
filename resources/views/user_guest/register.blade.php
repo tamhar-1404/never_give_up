f2722bd63f9ad4b676106fc50e357cb70ad01de3
tamhar-1404 <127166391+tamhar-1404@users.noreply.github.com>
2023-03-17 13:26:39 +0700

    register

--- resources/views/user_guest/register.blade.php
+++ resources/views/user_guest/register.blade.php
@@ -19,6 +19,7 @@
 body{
     /* background-color: #080710; */
     background-image: url("upload/slide3.jpg");
+    
 }
 .background{
     width: 430px;
@@ -139,64 +140,7 @@ button{
 
 	<!-- Container -->
 	<div id="container" >
-		<!-- Header
-		    ================================================== -->
-			<header class="clearfix ">
-
-				<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-					<div class="container">
-	
-						<a class="navbar-brand" href="/">
-							<img src="images/logo-black.png" alt="">
-						</a>
-	
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-							<span class="navbar-toggler-icon"></span>
-						</button>
-	
-						<div class="collapse navbar-collapse" id="navbarSupportedContent">
-							<ul class="navbar-nav mr-auto">
-								<li>
-									<a class=" " href="/">Home <i class="" aria-hidden="true"></i></a>
-								</li>
-								<li ><a  href="#">kategori <i class="fa fa-caret-down" aria-hidden="true"></i></a>
-									<div class="megadropdown">
-										<div class="dropdown-box">
-											
-											<ul class="dropdown-list">
-												<li><a href="/artikel">artikel</a></li>
-												<li><a href="/cerpen">cerpen</a></li>
-												<li><a href="/puisi">puisi</a></li>
-												<li><a href="/fotografi">fotografi</a></li>
-												<li><a href="/pantun">pantun</a></li>
-												<li><a href="/esai">esai</a></li>
-											</ul>
-										</div>
-										<div class="dropdown-box">
-											<ul class="dropdown-list">
-												<li><a href="/diary">diary</a></li>
-												<li><a href="/ilustrasi">ilustrasi</a></li>
-												<li><a href="/makalah">makalah</a></li>
-												<li><a href="/skripsi">skripsi</a></li>
-												<li><a href="/ilmiah">karya ilmiah</a></li>
-											</ul>
-										</div>
-										
-									</div>
-								</li>
-								
-								
-							</ul>
-							<ul class="navbar-nav ml-auto right-list">
-								<li><a href="/sing-page"><i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i> Sign In</a></li>
-								<li><a href="/sing-page"><i class="fa fa-user-o" aria-hidden="true"></i> Register</a></li>
-							</ul>
-							
-						</div>
-					</div>
-				</nav>
-			</header>
-			<!-- End Header -->
+		
 
 	
 
@@ -209,64 +153,56 @@ button{
                   <div class="row justify-content-center">
                     <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
       
-                      <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                     
       
                       <form class="mx-1 mx-md-4" method="POST" action="{{Route('create')}}" class="sign-form" enctype="multipart/form-data" >
                         @csrf
                         <div class="d-flex flex-row align-items-center mb-20">
-                          <i class=""><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
-                            <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
-                          </svg></i>
+                          
                           <div class="form-outline flex-fill mb-0">
                             <label class="form-label" for="email"> Email</label>
                             <input class="sign-form__input-text  @error('email') is-invalid @enderror" type="text" name="email" :value="old('email')" required placeholder="Enter your email" />
                           </div>
                         </div>
-						@error ('email')
-						<p class="text-black" style="color:black;">{{$message}}</p>
-						@enderror
+                        @error ('email')
+                        <p class="text-black" style="color:black;">{{$message}}</p>
+                        @enderror
       
                         <div class="d-flex flex-row align-items-center mb-20">
-                          <i class="" ><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
-                          </svg></i>
+                          
                           <div class="form-outline flex-fill mb-20">
                             <label class="form-label" for="username2">Username</label>
                             <input class="sign-form__input-text @error('username') is-invalid @enderror" type="text" name="username" :value="old('username')" required autofocus  id="name" placeholder="Enter your name" />
                           </div>
                         </div>
-						@error ('username')
-						<p class="text-black" style="color:black;">{{$message}}</p>
-						@enderror
+                        @error ('username')
+                        <p class="text-black" style="color:black;">{{$message}}</p>
+                        @enderror
       
                         <div class="d-flex flex-row align-items-center mb-10">
-                          <i class=""><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
-                            <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
-                          </svg></i>
+                        
                           <div class="form-outline flex-fill mb-0">
                             <label class="form-label" for="password2">Password</label>
                             <input class="sign-form__input-text   @error('password') is-invalid @enderror" type="password" name="password" required id="password" placeholder="Password" />
                           </div>
                         </div>
-						@error ('password')
-						<p class="text-black" style="color:black;">{{$message}}</p>
-						@enderror
+                        @error ('password')
+                        <p class="text-black" style="color:black;">{{$message}}</p>
+                        @enderror
 
 						
 
       
                         <div class="d-flex flex-row align-items-center mb-4">
-                          <i class=""><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
-                            <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-                          </svg></i>
+                          
                           <div class="form-outline flex-fill mb-0">
                             <label class="form-label" for="password2">konfirmasi password</label>
                             <input class="sign-form__input-text  @error('password_confirmation') is-invalid @enderror" type="password" name="password_confirmation" required id="password" placeholder="Password" />
                           </div>
                         </div>
-						@error ('password_confirmation')
-						<p class="text-black" style="color:black;">{{$message}}</p>
-						@enderror
+                        @error ('password_confirmation')
+                        <p class="text-black" style="color:black;">{{$message}}</p>
+                        @enderror
                        
                       
       
@@ -279,34 +215,11 @@ button{
                     </div>
                     <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
       
-                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                        class="img-fluid" alt="Sample image">
       
                     </div>
                   </div>
                 </div>
-            @endif
-    <form class="mx-1 mx-md-4" method="POST" action="{{Route('create')}}" class="sign-form" enctype="multipart/form-data" >
-        @csrf
-        <h3>Register </h3>
-
-        <label for="username">Email</label>
-        <input class="sign-form__input-text" type="text" name="email" :value="old('email')" required placeholder="Enter your email" />
-
-        <label for="password">Username</label>
-        <input class="sign-form__input-text" type="text" name="name" :value="old('name')" required autofocus  id="name" placeholder="Enter your name" />
-        
-        <label for="username">Password</label>
-        <input class="sign-form__input-text" type="password" name="password" required id="password" placeholder="Password" />
-
-        <label for="password">Konfirmasi Password</label>
-        <input class="sign-form__input-text" type="password" name="password_confirmation" required id="password" placeholder="Password" />
-       
-        <label for="password">foto Profil</label>
-        <input class="sign-form__input-text" type="file" name="foto" id="foto" placeholder="masukan  foto anda" />
-
-        <button  type="submit" id="submit_signup">Register</button>
-       Sudah Punya Akun?<a href="user_login" style="text-decoration-color: #23a2f6;">Login</a>
-    </form>
+           
+    
 </body>
 </html>
